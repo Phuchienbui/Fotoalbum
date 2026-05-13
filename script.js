4286fc94e506df4994b75b6151028af0332c783b
Hien <phuchienbui2@gmail.com>
2026-05-13 10:51:25 +0200

    refect: dialogIDS

---
+++ script.js
@@ -0,0 +1,15 @@
+const gallery = document.getElementById("gallery");
+const dialog = document.querySelector("dialog");
+const dialogTitle = document.getElementById("dialogTitle");
+const dialogCloseBtn = document.getElementById("dialogCloseBtn");
+const dialogLeftBtn = document.getElementById('dialogLeftBtn');
+const dialogRightBtn = document.getElementById('dialogRightBtn');
+const pCounter = document.getElementById('counter');
+
+let indexCounter = 0;
+
+function galleryRender() {
+    let galleryHtml = "";
+
+    galleryHtml += 
+}
