8c60dfbd5730272cfcde03912052db609e5bd179
Phuchienbui <phuchienbui2@gmail.com>
2026-05-14 00:00:13 +0200

    refec: styling

--- script.js
+++ script.js
@@ -1,17 +1,18 @@
 const images =
     [
-        { src: "Fotoalbum\imgPixabay/ahmetyuksek-swan-close-up-9767494_1920.jpg", alt: "Schwan mit Jungtier auf dem Wasser", title: "Schwanenfamilie auf dem See" },
-        { src: "Fotoalbum\imgPixabay\babelphotography-village-279013.jpg", alt: "Küstenort mit bunten Häusern auf einer Klippe am Meer", title: "Küstenstadt bei Abendlicht" },
-        { src: "Fotoalbum\imgPixabay\couleur-dandelion-10245975_1920.jpg", alt: "Löwenzahn mit Samen im Gegenlicht", title: "Löwenzahn im Wind" },
-        { src: "Fotoalbum\imgPixabay\daniil_kondrashin-autumn-9912502_1920.jpg", alt: "Frau mit roten Haaren", title: "Porträt einer Frau" },
-        { src: "Fotoalbum\imgPixabay\fotos-art-fine-art-9978188_1920.jpg", alt: "Nahaufnahme von Kirschblüten", title: "Kirschblüte im Frühling" },
-        { src: "Fotoalbum\imgPixabay\gom_su_cuong_duyen-ceramic-vase-10211596_1920.jpg", alt: "Porträt einer Frau neben einer Blumenvase", title: "Porträt mit Blumenarrangement" },
-        { src: "img/Property 1=moorente-8783210_1280.jpg.png", alt: "Eine Moorente auf dem Wasser", title: "Moorente" },
-        { src: "img/Property 1=sea-2563389_1280.jpg.png", alt: "Silhouette einer Person auf einem Felsen im Meer", title: "Der Mann auf dem Fels" },
-        { src: "img/Property 1=snow-bunting-6781122_1280.jpg.png", alt: "Eine Schneeammer im Wintergefieder sitzt auf einem dunklen Küstenfelsen", title: "Schneeammer" },
-        { src: "img/Property 1=snow-leopard-cubs-8039138_1280.jpg.png", alt: "junger Schneeleopard im Freien", title: "Baby Schneeleopard" },
-        { src: "img/Property 1=travel-8785493_1280.jpg.png", alt: "Panorama einer schneebedeckten Gebirgskette wärend die Sonne untergeht", title: "Sonnenuntergang in den Bergen" },
-        { src: "img/Property 1=winter-1675197_1280.jpg.png", alt: "Vereister Baum mitten in der Schneelandschaft", title: "Vereister Baum" }
+        { src: "./imgPixabay/ahmetyuksek-swan-close-up-9767494_1920.jpg", alt: "Schwan mit Jungtier auf dem Wasser", title: "Schwanenfamilie auf dem See" },
+        { src: "./imgPixabay/babelphotography-village-279013.jpg", alt: "Küstenort mit bunten Häusern auf einer Klippe am Meer", title: "Küstenstadt bei Abendlicht" },
+        { src: "./imgPixabay/couleur-dandelion-10245975_1920.jpg", alt: "Löwenzahn mit Samen im Gegenlicht", title: "Löwenzahn im Wind" },
+        { src: "./imgPixabay/daniil_kondrashin-autumn-9912502_1920.jpg", alt: "Frau mit roten Haaren", title: "Porträt einer Frau" },
+        { src: "./imgPixabay/fotos-art-fine-art-9978188_1920.jpg", alt: "Nahaufnahme von Kirschblüten", title: "Kirschblüte im Frühling" },
+        { src: "./imgPixabay/gom_su_cuong_duyen-ceramic-vase-10211596_1920.jpg", alt: "Porträt einer Frau neben einer Blumenvase", title: "Porträt mit Blumenarrangement" },
+        { src: "./imgPixabay/gruendercoach-robin-10205230_1920.jpg", alt: "Ein Rotkehlchen sitzt auf einem Ast mit Tannen-Nadeln", title: "Das ist ein Rotkehlchen" },
+        { src: "./imgPixabay/thibaultlamtran-vinyl-9617456_1920.jpg", alt: "Schallplatte auf einem Plattenspieler", title: "Vinyl auf dem Plattenspieler" },
+        { src: "./imgPixabay/guvo59-berry-tarts-9735441_1920.jpg", alt: "Törtchen mit Beeren und Puderzucker", title: "Beeren-Törtchen mit Puderzuckert" },
+        { src: "./imgPixabay/kyraxys-depth-10210903_1920.png", alt: "Silhouette eines Tauchers unter Wasser im Lichtstrahl", title: "Tauchgang ins Licht" },
+        { src: "./imgPixabay/storme22k-fennec-fox-9846388_1920.jpg", alt: "Schlafender Fuchs auf einem Stein", title: "Ein Moment der Ruhe" },
+        { src: "./imgPixabay/thibaultlamtran-vinyl-9617456_1920.jpg", alt: "Schallplatte auf einem Plattenspieler", title: "Vinyl auf dem Plattenspieler" },
+        { src: "./imgPixabay/veronika_andrews-anna-10217636_1920.jpg", alt: "Kolibri im Flug mit ausgebreiteten Flügeln", title: "Leichter Flug" }
     ]
 
 
