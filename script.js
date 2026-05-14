592ec035ec6b833b85bb78f0e4477cabb2a79fc2
Phuchienbui <phuchienbui2@gmail.com>
2026-05-15 01:15:55 +0200

    feat: style likebutton

--- script.js
+++ script.js
@@ -35,7 +35,9 @@ function renderGallery() {
             <li class="gallery_item">
                     <button onclick="openDialog(${i})" class="gallery__button" data-index="${i}">
                         <img class= "gallery__pic" src="${images[i].src}" alt="${images[i].alt}">
+                        <img class= "heart-icon" src="./svg/favorite.svg" alt="aria-hidden=true">
                     </button>
+                    
                 </li>
         `
     }
