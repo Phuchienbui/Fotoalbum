31b6c9045bf7ff180917b38df878561f9bd6e443
Phuchienbui <phuchienbui2@gmail.com>
2026-05-16 21:42:57 +0200

    fix:LikeBtn

--- script.js
+++ script.js
@@ -1,19 +1,19 @@
 const images =
     [
-        { src: "./imgkombri/ahmetyuksek-swan-close-up-9767494_1920.jpg", alt: "Schwan mit Jungtier auf dem Wasser", title: "Schwanenfamilie auf dem See" },
-        { src: "./imgkombri/husien_pro-iced-coffee-10260387_1920.jpg", alt: "Eiskaffee mit Milchschichten und Schaumkrone in einem Glas", title: "Erfrischender Iced Latte Macchiato" },
-        { src: "./imgkombri/couleur-dandelion-10245975_1920.jpg", alt: "Löwenzahn mit Samen im Gegenlicht", title: "Löwenzahn im Wind" },
-        { src: "./imgkombri/daniil_kondrashin-autumn-9912502_1920.jpg", alt: "Frau mit roten Haaren", title: "Porträt einer Frau" },
-        { src: "./imgkombri/fotos-art-fine-art-9978188_1920.jpg", alt: "Nahaufnahme von Kirschblüten", title: "Kirschblüte im Frühling" },
-        { src: "./imgkombri/gom_su_cuong_duyen-ceramic-vase-10211596_1920.jpg", alt: "Porträt einer Frau neben einer Blumenvase", title: "Porträt mit Blumenarrangement" },
-        { src: "./imgkombri/gruendercoach-robin-10205230_1920.jpg", alt: "Ein Rotkehlchen sitzt auf einem Ast mit Tannen-Nadeln", title: "Das ist ein Rotkehlchen" },
-        { src: "./imgkombri/guvo59-berry-tarts-9735441_1920.jpg", alt: "Törtchen mit Beeren und Puderzucker", title: "Beeren-Törtchen mit Puderzuckert" },
-        { src: "./imgkombri/kyraxys-depth-10210903_1920.jpg", alt: "Silhouette eines Tauchers unter Wasser im Lichtstrahl", title: "Tauchgang ins Licht" },
-        { src: "./imgkombri/storme22k-fennec-fox-9846388_1920.jpg", alt: "Schlafender Fuchs auf einem Stein", title: "Ein Moment der Ruhe" },
-        { src: "./imgkombri/thibaultlamtran-vinyl-9617456_1920.jpg", alt: "Schallplatte auf einem Plattenspieler", title: "Vinyl auf dem Plattenspieler" },
-        { src: "./imgkombri/veronika_andrews-anna-10217636_1920.jpg", alt: "Kolibri im Flug mit ausgebreiteten Flügeln", title: "Leichter Flug" },
-        { src: "./imgkombri/tehzeebkazmii-highland-cattle-10239253_1920.jpg", alt: "Porträt eines braunen schottischen Hochlandrinds mit langen Hörnern und zotteligem Fell auf einer grünen Wiese", title: "Majestätisches Hochlandrind" },
-    ]
+        { src: "./imgkombri/ahmetyuksek-swan-close-up-9767494_1920.jpg", alt: "Schwan mit Jungtier auf dem Wasser", title: "Schwanenfamilie auf dem See",liked:false},
+        { src: "./imgkombri/husien_pro-iced-coffee-10260387_1920.jpg", alt: "Eiskaffee mit Milchschichten und Schaumkrone in einem Glas", title: "Erfrischender Iced Latte Macchiato",liked:false},
+        { src: "./imgkombri/couleur-dandelion-10245975_1920.jpg", alt: "Löwenzahn mit Samen im Gegenlicht", title: "Löwenzahn im Wind",liked:false },
+        { src: "./imgkombri/daniil_kondrashin-autumn-9912502_1920.jpg", alt: "Frau mit roten Haaren", title: "Porträt einer Frau",liked:false },
+        { src: "./imgkombri/fotos-art-fine-art-9978188_1920.jpg", alt: "Nahaufnahme von Kirschblüten", title: "Kirschblüte im Frühling",liked:false },
+        { src: "./imgkombri/gom_su_cuong_duyen-ceramic-vase-10211596_1920.jpg", alt: "Porträt einer Frau neben einer Blumenvase", title: "Porträt mit Blumenarrangement",liked:false },
+        { src: "./imgkombri/gruendercoach-robin-10205230_1920.jpg", alt: "Ein Rotkehlchen sitzt auf einem Ast mit Tannen-Nadeln", title: "Das ist ein Rotkehlchen",liked:false },
+        { src: "./imgkombri/guvo59-berry-tarts-9735441_1920.jpg", alt: "Törtchen mit Beeren und Puderzucker", title: "Beeren-Törtchen mit Puderzuckert",liked:false },
+        { src: "./imgkombri/kyraxys-depth-10210903_1920.jpg", alt: "Silhouette eines Tauchers unter Wasser im Lichtstrahl", title: "Tauchgang ins Licht",liked:false },
+        { src: "./imgkombri/storme22k-fennec-fox-9846388_1920.jpg", alt: "Schlafender Fuchs auf einem Stein", title: "Ein Moment der Ruhe",liked:false },
+        { src: "./imgkombri/thibaultlamtran-vinyl-9617456_1920.jpg", alt: "Schallplatte auf einem Plattenspieler", title: "Vinyl auf dem Plattenspieler",liked:false },
+        { src: "./imgkombri/veronika_andrews-anna-10217636_1920.jpg", alt: "Kolibri im Flug mit ausgebreiteten Flügeln", title: "Leichter Flug",likedked:false },
+        { src: "./imgkombri/tehzeebkazmii-highland-cattle-10239253_1920.jpg", alt: "Porträt eines braunen schottischen Hochlandrinds mit langen Hörnern und zotteligem Fell auf einer grünen Wiese", title: "Majestätisches Hochlandrind",liked:false}
+    ];
 
 
 const gallery = document.getElementById("gallery");
@@ -38,12 +38,21 @@ function clickEvents(index) {
     } else{
         clearTimeout(clickTimer);
         clickTimer = null;
-        toggleLikeBtn();
+        toggleLikeBtn(index);
     }
 }
 
-function toggleLikeBtn(){
-    heartIcon.style.backgroundColor = "red";
+function toggleLikeBtn(index){
+  currentCounter = index;
+  images[currentCounter].liked = !images[currentCounter].liked;
+  const BTnNumber = document.getElementById(`Btn-${index}`);
+  const heart = BTnNumber.querySelector(".heart-icon");
+  
+  if (images[currentCounter].liked){
+    heart.src = "./svg/favoriteLiked.svg";
+  }else{
+    heart.src = "./svg/favorite.svg";
+  }
 }
 
 
@@ -53,11 +62,10 @@ function renderGallery() {
         galleryHtML +=
             `
             <li class="gallery_item">
-                    <button onclick="clickEvents(${i})" class="gallery__button" data-index="${i}">
+                    <button onclick="clickEvents(${i})" class="gallery__button" id="Btn-${i}">
                         <img class= "gallery__pic" src="${images[i].src}" alt="${images[i].alt}">
                         <img class= "heart-icon" src="./svg/favorite.svg" alt="aria-hidden=true">
                     </button>
-                    
                 </li>
         `
     }
